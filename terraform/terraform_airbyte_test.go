15757c0b051ef79032e5b9c63f138f64b8ac30fc
Sung Won Chung <sungwonchung3@gmail.com>
2021-04-29 10:56:34 -0500

    add comments

--- terraform/terraform_airbyte_test.go
+++ terraform/terraform_airbyte_test.go
@@ -1,5 +1,6 @@
 package test
 
+// download go modules
 // go get github.com/gruntwork-io/terratest/modules/terraform@v0.34.0
 
 import (
@@ -10,15 +11,20 @@ import (
 )
 
 func TestTerraformAirbyteDemo(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../terraform/",
 	})
 
+	// Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
 
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	output := terraform.Output(t, terraformOptions, "compute_engine_id")
-	assert.Equal(t, "projects/dbt-demos-sung/zones/us-central1-a/instances/airbyte-demo", output)
+	assert.Equal(t, "projects/dbt-demos-sung/zones/us-central1-a/instances/airbyte-demo", output) //TODO: add parameters for this
 }

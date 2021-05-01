d41516035abb017557270b1a7f56485c69d1b2a6
sungchun12 <sungwonchung3@gmail.com>
2021-05-01 16:31:35 -0500

    add default gcloud project id env var

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 
 # setup env vars
 export YOUR_GCP_PROJECT="dbt-demos-sung"
+export GOOGLE_CLOUD_PROJECT_ID="dbt-demos-sung"
 export INSTANCE_NAME="airbyte-demo"
 export YOUR_GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="../service_account.json"

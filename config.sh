c8c0c1dfc37957b96fae42cbbbf3e5e26b3dfac3
Sung Won Chung <sungwonchung3@gmail.com>
2021-05-03 11:47:15 -0500

    add optional terratest configs

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ export INSTANCE_NAME="airbyte-demo"
 export YOUR_GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="../service_account.json"
 
-# terratest configs
+# terratest configs for faster, local testing
 export SKIP_teardown=true
+# export SKIP_build_image=true
 export SKIP_cleanup_image=true

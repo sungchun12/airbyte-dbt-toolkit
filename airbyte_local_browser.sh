6fba246efc40d7680884a92039dbca406c13977a
Sung Won Chung <sungwonchung3@gmail.com>
2021-05-04 13:10:49 -0500

    better logs in the script

--- airbyte_local_browser.sh
+++ airbyte_local_browser.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-
-# remove anything running on port 8000 and 8001 for lingering airbyte webserver deployment
+echo "***********************"
+echo "Removing anything running on port 8000 and 8001 for lingering airbyte webserver deployment"
+echo "***********************"
 lsof -i:8000 -i:8001 -Fp | sed 's/^p//' | xargs kill -9
 
-# create ssh tunnel to run airbyte webserver in the local browser
+echo "***********************"
+echo "Creating ssh tunnel to run airbyte webserver in the local browser"
+echo "***********************"
 gcloud beta compute ssh $INSTANCE_NAME \
 --project=$GOOGLE_CLOUD_PROJECT_ID \
 --zone $YOUR_GCP_ZONE \
 -- -L 8000:localhost:8000 -L 8001:localhost:8001 -N -f
 
-# open the airbyte webserver in your local browser
+echo "***********************"
+echo "Opening the airbyte webserver in your local browser"
+echo "***********************"
 open http://localhost:8000/
+
+echo "***********************"
+echo "You should now see your airbyte webserver in your local browser!"
+echo "***********************"
+
+exit

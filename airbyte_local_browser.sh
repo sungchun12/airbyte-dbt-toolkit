903815605d314a505493e889258bfe039a427671
Sung Won Chung <sungwonchung3@gmail.com>
2021-05-10 15:23:26 -0500

    consistent vars with config.sh

--- airbyte_local_browser.sh
+++ airbyte_local_browser.sh
@@ -7,9 +7,9 @@ lsof -i:8000 -i:8001 -Fp | sed 's/^p//' | xargs kill -9
 echo "***********************"
 echo "Creating ssh tunnel to run airbyte webserver in the local browser"
 echo "***********************"
-gcloud beta compute ssh $INSTANCE_NAME \
---project=$GOOGLE_CLOUD_PROJECT_ID \
---zone $YOUR_GCP_ZONE \
+gcloud beta compute ssh $TF_VAR_name \
+--project=$TF_VAR_project \
+--zone $TF_VAR_zone \
 -- -L 8000:localhost:8000 -L 8001:localhost:8001 -N -f
 
 echo "***********************"
